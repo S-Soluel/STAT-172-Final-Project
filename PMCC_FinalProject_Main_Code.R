d7d218f1ac5a988bae799448e5ffb7f27b497d85
Samuel Solheim <97365420+S-Soluel@users.noreply.github.com>
2022-12-01 11:39:03 -0600

    Update PMCC_FinalProject_Main_Code.R
    I made several changes to our code for the X variable creation, please take a look when possible

--- PMCC_FinalProject_Main_Code.R
+++ PMCC_FinalProject_Main_Code.R
@@ -70,32 +70,29 @@ summary(cats$outcome_bin)
 # The number that was calculated matched the number of 1's in cats$outcome_bin. 
 # Binary Y variable has been created, now we can go into the next steps.
 
-# X VARIABLES --------------
+# CREATION OF ADDITIONAL X VARIABLES --------------
 
 # creating a season variable using the month the cat's outcome occurred
 # going by the generally agreed upon distribution of the seasons by month
-cats$season <- as.character(ifelse(cats$outcome_month == 12 | 
-                                   cats$outcome_month == 1 | 
-                                   cats$outcome_month == 2, "Winter",
-                            ifelse(cats$outcome_month == 3 | 
-                                   cats$outcome_month == 4 | 
-                                   cats$outcome_month == 5, "Spring",
-                            ifelse(cats$outcome_month == 6 | 
-                                     cats$outcome_month == 7 | 
-                                     cats$outcome_month == 8, "Summer",
-                            ifelse(cats$outcome_month == 9 | 
-                                   cats$outcome_month == 10 | 
-                                   cats$outcome_month == 11, "Fall", "N/A")))))
+cats$season_outcome <- as.character(ifelse(cats$outcome_month %in% c(12, 1, 2), "Winter", 
+                            ifelse(cats$outcome_month %in% c(3, 4, 5), "Spring",
+                            ifelse(cats$outcome_month %in% c(6, 7, 8), "Summer",
+                            ifelse(cats$outcome_month %in% c(9, 10, 11), "Fall",
+                            "N/A")))))
+                                   
 # check if it worked
-unique(cats$season)
+unique(cats$season_outcome)
 
 # change to an ordered factor
-cats$season <- factor(cats$season, 
-                      levels = c("Winter", "Spring", "Summer", "Fall"))
+cats$season_outcome <- factor(cats$season_outcome, 
+                      levels = c("Spring", "Summer", "Fall", "Winter"))
 # check if it worked
-table(cats$season)
+table(cats$season_outcome)
+str(cats)
+# season_outcome was correctly marked as a factor
 
-# creating a variable counting the number of characters in each name
+# creating a variable counting the number of characters in each name to 
+# explore the impact of during x variable selection
 str(cats$name)
 
 #convert to a character
@@ -103,15 +100,27 @@ cats$name <- as.character(cats$name)
 
 # create the numeric variable
 cats$name_length <- as.numeric(nchar(cats$name))
-# check if it worked
+# check if it worked as anticipated
 unique(cats$name_length)
 str(cats)
 
-# create a consolidated age variable when the cat's outcome occurs
+# Appears to have worked, noticed there are some cats that apparently
+# don't come in with names, as there is a length 0 name. Look into this further:
+table(cats$name_length)
+# It appears the vast majority of cats (12774) don't have names when they come in,
+# we can't really consolidate this into other groups, so it will be interesting to
+# see how this affects a cat's outcome. It could possibly mean that cat wasn't anyone's pet
+# before arriving at the shelter. 
+
+# create a consolidated age variable that says what age group a cat
+# is in when their outcome is reported
 unique(cats$age_upon_outcome)
 
 #convert to a character
 cats$age_upon_outcome <- as.character(cats$age_upon_outcome)
+# Note: Since we will be creating an age group variable, when we go ahead to x variable selection the age_upon_outcome variable
+#       will be left out by utilizing a subset of our data. 
+
 
 cats$outcome_age_group[cats$age_upon_outcome %in% c("2 weeks", "1 month", "3 weeks", "4 weeks", "3 days", "6 days", "1 week", "2 days", "5 days", "1 day", "1 weeks", "4 days", "0 years")] <- "0-1 months"
 # we decided to make the assumption that "0 years" should go into "0-1 months", primarily because that will be our most-common group
@@ -126,9 +135,10 @@ table(cats$outcome_age_group)
 # convert to an ordered factor
 cats$outcome_age_group <- factor(cats$outcome_age_group, 
                       levels = c("0-1 months", "1-3 months", "3-6 months", "6-12 months", "1-10 years", "10+ years"))
-# check if it worked
+# Check to make sure ordering worked
 table(cats$outcome_age_group)
-
+str(cats$outcome_age_group)
+# Looks all good! We can go ahead and proceed to the next stage
 
 
 
